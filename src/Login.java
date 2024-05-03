5fae55b6ca4363ee82d17d354f4de169c2cdf763
KareemZafan <kareemm.zafan@gmail.com>
2024-05-03 17:41:08 +0300

    remove file content

--- src/Login.java
+++ src/Login.java
@@ -1,20 +1,5 @@
 
 // This feature for login automation [Mostafa, Farse]
-kfkskf
-kfkkfd
-kdfkfsddf
-ksdffsddfk
-ksdf
-ksd
-fsdfksfsflsdfks
-ksfsffk
-fsddfksdfs'kd
-ffs
-fsfsfsf
-
-jsjfsjf
-sdjfsjfjs
-jdfsjfsfl
 
 
 
